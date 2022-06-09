2cf454bea8b2c2be807fb3d4d96c7796ef5721e7
wonmoLee <leewonmo94@gmail.com>
2022-06-09 22:56:41 +0900

    user schema 정의

---
+++ model/user.js
@@ -0,0 +1,28 @@
+/**
+ * Author: wonmoLee 
+ * Date: 2022.06.09
+ * GitHub: https://github.com/wonmoLee
+ * Blog: https://wonmolee.github.io
+ * 
+ * - Revision history -
+ * 
+ */
+
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    id: {type: String, required: true},
+    password: {type: String, require: true},
+    token: String,
+    agoToken: String,
+    friends: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    friendReceiveRequests: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    friendRequests:[{type:Schema.Types.ObjectId, ref:'User'}],
+    room:[{type:Schema.Types.ObjectId, ref:'Room'}],
+    inviteRooms:[{type:Schema.Types.ObjectId, ref:'Room'}],
+    CreatedAt:{type: Date, default: Date.now},
+    UpdatedAt:{type: Date, default: Date.now},
+});
+
+module.exports = mongoose.model('User', UserSchema);
